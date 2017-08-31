8bb9edb5cb592a202cc15c779d809e6407cc63ec
Richard Maya <richard.maya94@gmail.com>
2017-08-31 16:53:45 -0500

    Modal Window Updated

---
+++ index.php
@@ -0,0 +1,359 @@
+<?php $form_path='assets/formoid/form.php'; require_once $form_path; ?>
+<!DOCTYPE html>
+<!--**********************************************************************
+* FILENAME :        INATEK | Index             
+*
+* DESCRIPTION :
+*       Web site dedicated to fully describe all services and products
+*       offered by Inatek; as well as its own techonologies. 
+*
+*       Copyright Inatek SA de CV © All rights reserved.
+* 
+* AUTHOR :    Richard Maya
+* START DATE :    03 Aug 17
+*
+* VERSION : 1.0
+*
+*-->
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- SITE TITLE
+    ================================================== -->
+    <title>Ingeniería en Tecnología Ambiental | INATEK</title>
+    
+    <!-- SEO INFO
+    ================================================== -->
+    <meta name="description" content="Somos una empresa orgullosamente mexicana dedicada al desarrollo de soluciones tecnológicas específicas y orientadas al cuidado del ambiente y desarrollo de proyectos tecnológicos innovadores.">
+    <meta name="keywords" content="sustainable architecture, desarrollo sustentable, construccion sustentable,sustainable development,inatek, tecnosólido, urbano, residuos sólidos urbanos">
+    <meta name="copyright" content="Copyright © 2017 Inatek S.A. de C.V.">
+    <meta name="author" content="Richard Maya">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta http-equiv="Cache-Control" content="max-age=2592000">
+    
+    <!-- FAVICONS
+    ================================================== -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+    
+    <!-- STYLESHEETS
+    ================================================== -->
+    <link rel="stylesheet" href="assets/tether/css/tether.min.css">
+    <link rel="stylesheet" href="assets/animate.css/animate.min.css">
+    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+  
+<!-- NAV BAR
+================================================== -->
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="index.php">
+        <img src="gph/InatekLogo-01.svg" class="hidden-sm-down" alt="inatek-logo" height="65">
+        <img src="gph/inatek-isotipo.png" class="hidden-md-up" alt="inatek-logo" height="40">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="inatek.html">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tecnologia-inatek.html">Tecnologías</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="servicios-inatek.html">Servicios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Productos</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+  
+  
+<!-- HERO COVER
+================================================== -->
+<section class="hero parallax-window d-flex align-items-center" data-parallax="scroll" data-image-src="Imgs/nosotros.png">
+    <div class="container">
+        <div class="row">
+            <div class="col-10 offset-1 col-md-6 offset-md-3">
+                <img src="gph/inatek-logo-negativo.png" alt="inatek-logo" class="img-fluid wow fadeInUp">
+                <br>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-10 offset-1 col-md-8 offset-md-2 justify-content-center text-center wow fadeInUp">
+                <p class="lead">Somos una empresa orgullosamente mexicana dedicada al desarrollo de soluciones tecnológicas específicas y orientadas al cuidado del ambiente.</p>
+            </div>
+        </div>
+        
+        <div class="space15vh"></div>        
+    </div>
+</section>
+
+
+<!-- SERVICES
+================================================== -->
+<section id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 offset-0 col-md-4 offset-md-0 wow fadeInLeft">
+                <a href="tecnologia-inatek.html">
+                    <img src="gph/Tec-01.png" alt="tecnologias-inatek" class="img-fluid hvr-bob" style="margin-bottom: 50px;">
+                </a>
+            </div>
+            <div class="col-12 offset-0 col-md-4 offset-md-0 wow fadeInUp">
+                <a href="#">
+                    <img src="gph/Serv-01.png" alt="nosotrs-inatek" class="img-fluid hvr-bob">
+                </a>
+            </div>
+            <div class="col-12 offset-0 col-md-4 offset-md-0 wow fadeInRight">
+                <a href="#">
+                    <img src="gph/Productos-01.png" alt="servicios-inatek" class="img-fluid hvr-bob" style="margin-bottom: 50px;">
+                </a>
+            </div>
+           <!-- <div class="col-6 offset-0 col-md-3 offset-md-0 wow fadeInRight">
+                <a href="inatek.html">
+                    <img src="gph/Nosotros-01.png" alt="servicios-inatek" class="img-fluid hvr-bob">
+                </a>
+            </div>-->
+        </div>
+    </div>
+</section>
+ 
+ 
+<!-- MISION
+================================================== -->
+ <section id="mision">
+     <div class="container">
+         <div class="row justify-content-center">
+             <h1 class="ina-title wow fadeInUp">Misión</h1>
+         </div>
+         
+         <div class="row">
+             <div class="col-12 col-md-5 d-flex align-items-center">
+                 <div class="ina-box">
+                     <p class="green-text text-center wow fadeInLeft">Proveer materiales amigables al medio ambiente, a costos competitivos por medio del desarrollo de innovaciones tecnológicas, aprovechando el total de los residuos sólidos urbanos para su transformación, valorización y contribución a la reducción significativa de la emisión de gases efecto invernadero "GEI", para mejorar nuestra calidad de vida y de las generaciones futuras.</p>
+                 </div>
+             </div>
+             <div class="col-12 col-md-7">
+                 <img src="Imgs/PagMision.png" alt="inatek-mision" style="width: 110%; margin-left: -10%;" class="hidden-sm-down wow fadeInRight">
+                 <img src="Imgs/PagMision.png" alt="inatek-mision" class="img-fluid hidden-md-up wow fadeInRight">
+             </div>
+         </div>
+     </div>
+ </section>
+ 
+ 
+<!-- VISION
+================================================== -->
+<section id="vision">
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1 class="ina-title wow fadeInUp">Visión</h1>
+        </div>
+        
+        <div class="row">
+            <div class="col-12 col-md-7">
+                <img src="Imgs/PagVision.png" alt="inatek-vision" class="img-fluid hidden-md-up wow fadeInLeft">
+                <img src="Imgs/PagVision.png" alt="inatek-vision" style="width: 115%;" class="hidden-sm-down wow fadeInLeft">
+            </div>
+            <div class="col-12 col-md-5 d-flex align-items-center">
+                <div class="ina-box">
+                    <p class="green-text text-center wow fadeInRight">Ser una empresa sustentable dedicada a proporcionar soluciones amigables al ambiente, que ofrezca y comercialice productos y tecnologías verdes.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- OUR ACCOMPLISHMENTS
+================================================== -->
+<section id="ciel">
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1 class="ina-title wow fadeInUp">Nuestros Logros</h1>
+            <div class="space15vh hidden-md-up"></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-5 d-flex align-items-center">
+                <div class="ina-box">
+                    <img src="gph/Logos/ciel.jpg" alt="ciel-transformadora-inatek" id="ciel-logo" class="wow fadeInLeft">
+                    <p class="green-text text-center wow fadeInLeft">Nuestra participación fue para desarrollar el proyecto denominado MATERIALES RSU, el cual fabrica tabiques y materiales de construcción sustentables por medio de la tecnología única e innovadora en México llamada FEASABILITY</p>
+                    <p class="green-text text-center wow fadeInLeft">Conseguimos estar entre los 4 ganadores del concurso Transformadora CIEL.</p>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <img src="Imgs/Ciel.png" alt="inatek-ciel-transformadora" class="img-fluid hidden-md-up wow fadeInRight">
+                <img src="Imgs/Ciel.png" alt="inatek-ciel-transformadora" class="hidden-sm-down wow fadeInRight" style="width: 110%; margin-left: -10%;">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="cleantech">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 offset-0 col-md-8 offset-md-2">
+                <div class="ina-box">
+                    <img src="gph/Logos/cleantech.jpg" alt="cleantech-challenge-mexico-inatek" id="cleantech-logo" class="wow fadeInUp">
+                    <p class="green-text text-center wow fadeInUp">En la versión del concurso CTCM-2013 se desarrolló y evaluó el proyecto "INATEK - Creación de métodos no convencionales y desarrollo de tecnologías para procesar los Residuos Sólidos Urbanos y transformarlos en materiales amigables con el ambiente, especialmente en la industria de la construcción."; quedando entre las mejores propuestas de más de 1000 a nivel nacional.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<img src="gph/Hoja_Azul.png" alt="" style="width: 100%;">
+
+
+<!-- CONTACT FORM
+================================================== -->
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.4577469651445!2d-99.621172!3d19.2624502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd8bcdb8c8cfd9%3A0x1567b3834ebe7508!2sInatek!5e0!3m2!1sen!2smx!4v1501798430712" width="100%" height="430" frameborder="0" style="border:0;" allowfullscreen class="wow fadeInLeft"></iframe>
+            </div>
+            <div class="col-md-5">
+                <h3 class="wow fadeInUp">Formulario de Contacto</h3>
+                <br>
+                <form method="post" class="formoid-solid-blue">
+                    
+                    <!-- Name -->
+                    <div class="form-group row">
+                        <label for="nombre" class="col-2 col-form-label wow fadeInUp">Nombre<sup>*</sup></label>
+                        <div class="col-9 offset-1">
+                            <input class="form-control" type="text" name="input" required="required" placeholder="Nombre">
+                        </div>
+                    </div>
+                    
+                    <!-- Email -->
+                    <div class="form-group row">
+                        <label for="correo" class="col-2 col-form-label wow fadeInUp">Correo<sup>*</sup></label>
+                        <div class="col-9 offset-1">
+                            <input class="form-control" type="email" name="email" required="required" placeholder="Correo">
+                        </div>
+                    </div>
+                    
+                    <!-- Phone -->
+                    <div class="form-group row">
+                        <label for="telefono" class="col-2 col-form-label wow fadeInUp">Teléfono</label>
+                        <div class="col-9 offset-1">
+                            <input class="form-control" type="tel" pattern="[+]?[\.\s\-\(\)\*\#0-9]{3,}" maxlength="24" name="phone" placeholder="Teléfono">
+                        </div>
+                    </div>
+                    
+                    <!-- Message -->
+                    <div class="form-group">
+                        <label for="mensaje" class="wow fadeInUp">Mensaje<sup>*</sup></label>
+                        <textarea class="form-control" name="textarea" cols="20" rows="5" required="required" placeholder="Déjanos un mensaje."></textarea>
+                    </div>
+                    
+                    <div class="d-flex justify-content-center">
+                        <button class="btn btn-primary" type="submit" role="button" value="Enviar">Enviar</button>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+        
+        <br>
+        
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="ina-box" style="background: white;">
+                    <h5 class="wow fadeInUp text-center">Datos de Contacto</h5>
+                    <p class="wow fadeInUp text-center">
+                        Av. Benito Juárez Norte 907 Int. 102 <br>
+                        Col. La Purísima, CP: 52169, Metepec, Méx. <br>
+                        Tel: <a href="tel:01-722-212-2223">01 (722) 212-2223</a> <br>
+                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="tel:01-722-235-1201">2-35-12-01</a> <br>
+                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="tel:01-722-219-5959">2-19-59-59</a> <br>
+                        <a href="mailto:contacto@inatek.com.mx">contacto@inatek.com.mx</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+ 
+  
+<!-- FOOTER
+================================================== --> 
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 text-left">
+                <p>Copyright 2017 &copy; Inambtec S.A. de C.V. Todos los derechos reservados.</p>
+            </div>
+            <div class="col-4 text-right">
+                <a href="https://www.facebook.com/Inatek-497193530321422/">
+                    <i class="fa fa-facebook fa-lg" aria-hidden="true"></i>
+                </a>
+                <a href="https://twitter.com/INATEKrsu">
+                    <i class="fa fa-twitter fa-lg" aria-hidden="true"></i>
+                </a>
+                <a href="https://www.youtube.com/channel/UCdAwbhqMP6xkhMPd6d_lOhg">
+                    <i class="fa fa-youtube fa-lg" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+        
+        <div class="row justify-content-center" id="developer">
+            <p class="text-center">Desarrollado por <a href="http://www.cintegran.com.mx">GEM Cintegran</a></p>
+        </div>
+           
+        <div class="row justify-content-center" style="visibility: hidden; display: none;">
+            <p class="text-center">Desarrollado por <a href="http://meraki-creative.studio" rel="author">Meraki Creative Studio</a></p>
+        </div>
+    </div>
+</footer>
+
+   
+<!-- SCRIPTS
+================================================== -->
+<script src="assets/jquery/jquery.min.js"></script>
+<script src="assets/tether/js/tether.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="assets/parallax.js/parallax.min.js"></script>
+<script src="assets/wow/wow.min.js"></script>
+
+<script type="text/javascript">
+/* Parallax */
+$('.parallax-window').parallax({
+    imageSrc: 'Imgs/nosotros.png',
+    positionY: 'bottom'
+});
+    
+/* Wow Effect */
+var wow = new WOW(
+  {
+    boxClass:     'wow',     
+    animateClass: 'animated', 
+    offset:       50,         
+    mobile:       false,    
+    live:         true,       
+    callback:     function(box) {
+    },
+    scrollContainer: null
+  }
+);
+wow.init();
+</script>
+</body>
+</html>
